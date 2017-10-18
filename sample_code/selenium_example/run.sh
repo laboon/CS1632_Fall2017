c868e8c85866a79d7c64894384d499e4676eca92
laboon <laboon@gmail.com>
2017-10-18 09:24:23 -0400

    adsa

---
+++ sample_code/selenium_example/run.sh
@@ -0,0 +1,3 @@
+javac -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar *.java
+
+java -cp .:selenium-java-2.52.0.jar:selenium-server-standalone-2.52.0.jar:hamcrest-core-1.3.jar:junit-4.12.jar RedditTestRunner
